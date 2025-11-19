3cb6ca36fe43d070234fd1f1d6e8b8bbdede08fb
Lanthan <danilkor4444@gmail.com>
2025-11-19 12:39:45 +0100

    init + day1

---
+++ 2015/1/day1p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	freopen("../input.txt", "r", stdin);
+	string input;
+	cin >> input;
+	int floor = 0;
+	for (unsigned int i = 0; i < input.size(); i++) {
+		if (input[i] == '(') {
+			floor += 1;
+		}
+		else if (input[i] == ')') {
+			floor -= 1;
+		}
+	}
+	cout << floor << "\n";
+}
