a4c9c7587fce0e782306f8191f97c0f254bfc74d
Lanthan <danilkor4444@gmail.com>
2025-12-04 08:46:05 +0100

    2025 4 12 with R

---
+++ 2025/4/p1.R
@@ -0,0 +1,80 @@
+lines <- readLines("./input.txt")
+
+matrix <- matrix()
+matrix <- matrix[-c(1)]
+for (line in lines) {
+    matrix <- rbind(matrix, unlist(strsplit(trimws(line), '')))
+}
+#row, col
+directions <- matrix(c(c(1,1), c(1,0), c(1,-1), c(0,1), c(0,-1), c(-1,1), c(-1,-1), c(-1,0)), ncol=2, byrow = TRUE)
+
+rolls_number <- 0
+moveable <- 0
+mov <- list()
+for (row in seq_along(lines)) {
+    for (col in 1:nchar(lines[1])) {
+        aligned <- 0
+        if (matrix[row,col] != '@') {
+            next
+        }
+        for (i in 1:8) {
+            tr <- row + directions[i,1]
+            tc <- col + directions[i,2]
+            tryCatch(
+                {
+                    tr <- row + directions[i,1]
+                    tc <- col + directions[i,2]
+                    if (matrix[tr, tc] == '@') {
+                        aligned <- aligned + 1
+                    }
+                    # print(paste("ROW: ", row, " COL: ", col))
+                }, error=function(e) {}
+            )
+        }
+
+        # tryCatch({if (matrix[row+1, col+1] == '@'){
+        #     aligned <- aligned + 1
+        #     paste("ROW: ", row, " COL: ", col)
+        # }}, error=function(e){})
+        # tryCatch({if (matrix[row+1, col-1] == '@'){
+        #     aligned <- aligned + 1
+        #     paste("ROW: ", row, " COL: ", col)
+        # }}, error=function(e) {})
+        # tryCatch({if (matrix[row+1, col] == '@') {
+        #     aligned <- aligned + 1
+        #     paste("ROW: ", row, " COL: ", col)
+        # }}, error=function(e) {})
+        # tryCatch({if (matrix[row, col+1] == '@') {
+        #     aligned <- aligned + 1
+        #     paste("ROW: ", row, " COL: ", col)
+        # }}, error=function(e) {})
+        # tryCatch({if (matrix[row, col-1] == '@') {
+        #     aligned <- aligned + 1
+        #     paste("ROW: ", row, " COL: ", col)
+        # }}, error=function(e) {})
+        # tryCatch({if (matrix[row-1, col+1] == '@') {
+        #     aligned <- aligned + 1
+        #     paste("ROW: ", row, " COL: ", col)
+        # }}, error=function(e) {})
+        # tryCatch({if (matrix[row-1, col-1] == '@') {
+        #     aligned <- aligned + 1
+        #     paste("ROW: ", row, " COL: ", col)
+        # }}, error=function(e) {})
+        # tryCatch({if (matrix[row-1, col] == '@') {
+        #     aligned <- aligned + 1
+        #     paste("ROW: ", row, " COL: ", col)
+        # }}, error=function(e) {})
+
+        if (aligned < 4) {
+            moveable <- moveable + 1
+            mov <- append(mov, list(list(row,col)))
+        }
+    }
+}
+
+for (e in mov){
+    e <- unlist(e)
+    matrix[e[1],e[2]] <- "X"
+}
+
+print(moveable)
